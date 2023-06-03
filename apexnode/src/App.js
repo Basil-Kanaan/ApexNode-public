978b1113cc9bff46a851935b56cb4d9cc12e3ffd
Bb0lt <basil.kanaan@mail.utoronto.ca>
2023-06-02 21:29:37 -0400

    editing homepage text and ran npm start successfully

--- apexnode/src/App.js
+++ apexnode/src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Hopefully when i run the app this text shows.
         </p>
         <a
           className="App-link"
