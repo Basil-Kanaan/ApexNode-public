bd792b2c9a1e036afc04d539115e1f8c7a550f38
Bb0lt <basil.kanaan@mail.utoronto.ca>
2023-08-17 21:57:09 -0400

    added padding to main page

--- src/components/Dashboard/ClientDashboard.js
+++ src/components/Dashboard/ClientDashboard.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
-import { useAuth } from '../../AuthContext';
-import { Button, Divider, Typography} from "@mui/material";
-import { Content } from './styles';
+import React, {useState} from 'react';
+import {useAuth} from '../../AuthContext';
+import {Button, Divider, Typography} from "@mui/material";
+import {Content, Page} from './styles';
 import ProjectCard from "../ProjectCard";
 import DBNavBar from "./DBNavBar";
 
 const ClientDashboard = () => {
-    const { userRole } = useAuth();
+    const {userRole} = useAuth();
 
     const handleCreateProject = () => {
         // Implement logic to create a new project
@@ -21,30 +21,32 @@ const ClientDashboard = () => {
                     <div>
                         <DBNavBar/>
 
-                        {selectedTab === 0 && (
-                            <div>
-                                {/* Content for "Projects" tab */}
-                                <Typography variant="h4">Projects</Typography>
-                                <Divider sx={{ my: 2 }} />
-                                <Button variant="outlined" color="primary" onClick={handleCreateProject}>
-                                    Create New Project
-                                </Button>
+                        <Page>
+                            {selectedTab === 0 && (
+                                <div>
+                                    {/* Content for "Projects" tab */}
+                                    <Typography variant="h4">Projects</Typography>
+                                    <Divider sx={{my: 2}}/>
+                                    <Button variant="outlined" color="primary" onClick={handleCreateProject}>
+                                        Create New Project
+                                    </Button>
 
-                                {/* Display projects and their details here */}
-                                <ProjectCard/>
-                            </div>
-                        )}
+                                    {/* Display projects and their details here */}
+                                    <ProjectCard/>
+                                </div>
+                            )}
 
 
+                            {selectedTab === 2 && (
+                                <div>
+                                    {/* Content for "Settings" tab */}
+                                    <Typography variant="h4">Settings</Typography>
+                                    <Divider sx={{my: 2}}/>
 
-                        {selectedTab === 2 && (
-                            <div>
-                                {/* Content for "Settings" tab */}
-                                <Typography variant="h4">Settings</Typography>
-                                <Divider sx={{ my: 2 }} />
+                                </div>
+                            )}
+                        </Page>
 
-                            </div>
-                        )}
                     </div>
                 </Content>
             )}
--- src/components/Dashboard/DBNavBar.js
+++ src/components/Dashboard/DBNavBar.js
@@ -1,40 +1,49 @@
-import React, { useState }  from 'react';
-import {AppBar, Tab, Tabs, Toolbar} from "@mui/material";
+import React, {useState} from 'react';
+import {AppBar, Tab, Tabs, Toolbar} from '@mui/material';
+import {Folder, Message, Settings} from '@mui/icons-material'; // Import icons
 
 const DBNavBar = () => {
     const [selectedTab, setSelectedTab] = useState(0);
+
+    const tabs = [
+        {label: 'Projects', icon: <Folder/>},
+        {label: 'Messages', icon: <Message/>},
+        {label: 'Settings', icon: <Settings/>},
+    ];
 
     const handleTabChange = (event, newValue) => {
         setSelectedTab(newValue);
     };
 
     return (
-        <AppBar position="static" sx={{ background: "linear-gradient(to top, #29293d, #6e1423);", marginBottom: "1rem"}}>
-            <Toolbar sx={{ justifyContent: 'center' }}>
+        <AppBar
+            position="static"
+            sx={{
+                background: 'linear-gradient(to top, #29293d, #6e1423)',
+                marginBottom: '1rem',
+            }}
+        >
+            <Toolbar sx={{justifyContent: 'flex-end'}}>
                 <Tabs
                     value={selectedTab}
                     onChange={handleTabChange}
                     textColor="inherit"
                     indicatorColor="primary"
                 >
-                    <Tab
-                        label="Projects"
-                        sx={{
-                            fontSize: '1.1rem',
-                        }}
-                    />
-                    <Tab
-                        label="Messages"
-                        sx={{
-                            fontSize: '1.1rem',
-                        }}
-                    />
-                    <Tab
-                        label="Settings"
-                        sx={{
-                            fontSize: '1.1rem',
-                        }}
-                    />
+                    {tabs.map((tab, index) => (
+                        <Tab
+                            key={index}
+                            icon={tab.icon}
+                            sx={{
+                                fontSize: '1.1rem',
+                                transition: 'transform 0.2s, color 0.2s',
+                                '&:hover': {
+                                    transform: 'scale(1.1)',
+                                    color: '#FFF',
+                                },
+                            }}
+                        />
+                    ))}
                 </Tabs>
             </Toolbar>
         </AppBar>
