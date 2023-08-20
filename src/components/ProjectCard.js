f9d5c0a636b0c86b52bda87c6552fe62ea0840b9
Bb0lt <basil.kanaan@mail.utoronto.ca>
2023-08-20 01:17:02 -0400

    added create project functionality

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,15 +1,49 @@
 import React from 'react';
-import {Card, CardContent, Container, Typography} from "@mui/material";
+import { Card, CardContent, Container, Typography, IconButton, Menu, MenuItem } from '@mui/material';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
-const ProjectCard = (props) => {
+const ProjectCard = ({ id, title, progress, developers, description, onDelete }) => {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleMenuClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+    };
+
+    const handleDeleteClick = () => {
+        onDelete();
+        handleMenuClose();
+    };
+
     return (
         <div>
-            <Container sx={{mt: 2}}>
+            <Container sx={{ mt: 2 }}>
                 <Card>
                     <CardContent>
-                        <Typography variant="h6">Project Title</Typography>
-                        <Typography variant="body2">Progress: 40%</Typography>
-                        <Typography variant="body2">Developers: Developer A, Developer B</Typography>
+                        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                            <Typography variant="h6">{title}</Typography>
+                            <IconButton
+                                aria-controls={`project-card-menu-${id}`}
+                                aria-haspopup="true"
+                                onClick={handleMenuClick}
+                            >
+                                <MoreVertIcon />
+                            </IconButton>
+                            <Menu
+                                id={`project-card-menu-${id}`}
+                                anchorEl={anchorEl}
+                                open={Boolean(anchorEl)}
+                                onClose={handleMenuClose}
+                            >
+                                <MenuItem onClick={handleDeleteClick}>Delete</MenuItem>
+                            </Menu>
+                        </div>
+                        <Typography variant="body2">Progress: {progress}%</Typography>
+                        <Typography variant="body2">Developers: {developers}</Typography>
+                        <Typography variant="body2">{description}</Typography>
                         {/* Add more project details here */}
                     </CardContent>
                 </Card>
---
+++ src/pages/App/Projects.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { Typography, Divider, Button } from '@mui/material';
+import ProjectCard from '../../components/ProjectCard';
+
+const Projects = () => {
+    const [projects, setProjects] = useState([]); // State to store projects
+
+    const handleCreateProject = () => {
+        // Create a new project object with some example data
+        const newProject = {
+            id: projects.length + 1, // Incremental ID
+            title: `New Project ${projects.length + 1}`,
+            description: 'This is a new project description.',
+        };
+
+        // Update the projects state with the new project
+        setProjects([...projects, newProject]);
+    };
+
+    const handleDeleteProject = (projectId) => {
+        setProjects(projects.filter(project => project.id !== projectId));
+    };
+
+    return (
+        <div>
+            {/* Content for "Projects" tab */}
+            <Typography variant="h4">Projects</Typography>
+            <Divider sx={{ my: 2 }} />
+            <Button variant="outlined" color="primary" onClick={handleCreateProject}>
+                Create New Project
+            </Button>
+
+            {/* Display projects and their details here */}
+            {projects.map(project => (
+                <ProjectCard
+                    key={project.id}
+                    onDelete={() => handleDeleteProject(project.id)}
+                    title={project.title}
+                    progress={project.progress}
+                    developers={project.developers}
+                    description={project.description}
+                />
+            ))}
+        </div>
+    );
+};
+
+export default Projects;
