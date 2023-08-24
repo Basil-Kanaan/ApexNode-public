dccc2fe758d82b7f41489e6c6802774f62cab45c
Bb0lt <basil.kanaan@mail.utoronto.ca>
2023-08-23 22:59:52 -0400

    added routes for authenticated api requests

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const User = require('../models/Users');
+
+const router = express.Router();
+
+// Signup route
+router.post('/signup', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = new User({
+            email,
+            password: hashedPassword,
+        });
+        await newUser.save();
+
+        res.status(201).json({ message: 'User registered successfully' });
+    } catch (error) {
+        res.status(500).json({ error: 'An error occurred' });
+    }
+});
+
+// Login route
+router.post('/login', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
+
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if (!passwordMatch) {
+            return res.status(401).json({ error: 'Invalid credentials' });
+        }
+
+        res.status(200).json({ message: 'Login successful' });
+    } catch (error) {
+        res.status(500).json({ error: 'An error occurred' });
+    }
+});
+
+module.exports = router;
